5095a426333c2f7b1b1795ccc1b50e8305de9334
Ibrahima <>
2022-12-06 00:40:25 -0500

    made addtask into a modal

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,18 +1,48 @@
-import { Button, HStack, Input, useToast } from '@chakra-ui/react';
-import { useState } from 'react';
-import supabase from '../supabase';
+import { 
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  VStack,
+  Button, 
+  useDisclosure, 
+  useToast 
+} from '@chakra-ui/react';
+import { useState } from 'react'
+import supabase from '../supabase'
 
 export default function AddTask() {
-  const [text, setText] = useState('');
+  //Database
+  const {isOpen, onOpen, onClose} = useDisclosure() //For the modal's open/close
+  
+
+  //Page
+  const [task, setTask] = useState({title: "", text: ""})
+  const {title, text} = task
   const [loading, setLoading] = useState(false);
   const toast = useToast();
 
-  async function handleSubmit(e) {
+  async function saveTask(e) {
     e.preventDefault();
     setLoading(true);
-    const { data, error } = await supabase.from('todos').insert([{ text }]);
+
+    const { error } = await supabase
+        .from('todos') //Table name
+        .insert([
+            {title, text} //Columns
+        ])
+        console.log(task)
+      
+    
+    //Finishing tasks
     setLoading(false);
-    setText('')
+    setTask({title: "", text: ""})
 
     toast({
       title: error || 'task added',
@@ -21,23 +51,43 @@ export default function AddTask() {
       duration: 2000,
       isClosable: true,
     })
+
+    onClose();
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <HStack my="4" h="45">
-        <Input
-          h="100%"
-          variant="filled"
-          placeholder="Do the laundry"
-          value={text}
-          onChange={e => setText(e.target.value)}
-          disabled={loading}
-        />
-        <Button colorScheme="blue" px="10" h="100%" type="submit" isLoading={loading} loadingText="Adding...">
-          Add
-        </Button>
-      </HStack>
-    </form>
+    <>
+        <Button onClick={onOpen} colorScheme='blue' p='10px'>Add Task</Button>
+
+        <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
+            <ModalOverlay>
+            <ModalContent>
+              <ModalHeader>This is where something goes</ModalHeader>
+              <ModalBody>
+              <VStack p='25px'>
+              <FormControl>
+                <FormLabel>Title</FormLabel>
+                <Input type='text' 
+                    value={title}
+                    onChange={e => setTask({...task, title: e.target.value})}
+                />
+                <FormHelperText>Choose a title that's fun and concise!</FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Task Details</FormLabel>
+                <Input type='text' 
+                    value={text}
+                    onChange={e => setTask({...task, text: e.target.value})}
+                />
+                <FormHelperText>Explain your task in as little (or as much ;-) ) detail as you need!</FormHelperText>
+              </FormControl>
+              <Button colorScheme='blue' onClick={saveTask}>Add</Button>
+              </VStack>
+              </ModalBody>
+              <ModalCloseButton />
+          </ModalContent>
+          </ModalOverlay>
+        </Modal>
+    </>
   );
 }
