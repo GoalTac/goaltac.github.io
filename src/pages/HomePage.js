18b92dfc92513f313c1c556ec657ca321add612d
Ibrahima Capo-Chichi <40813947+icapochi@users.noreply.github.com>
2022-12-11 13:51:40 -0500

    Fixed element alignmentse

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,13 @@
+import { VStack } from '@chakra-ui/react';
 import AddTask from '../components/AddTask';
 import TaskList from '../components/TaskList';
 
 function HomePage() {
   return (
-    <div>
+    <VStack justifyContent={'center'}>
       <AddTask />
       <TaskList />
-    </div>
+    </VStack>
   );
 }
 
