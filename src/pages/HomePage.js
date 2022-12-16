abe4470b699262fadc371bc13dc7bfbfd2659f5f
Ibrahima Capo-Chichi <40813947+icapochi@users.noreply.github.com>
2022-12-16 15:38:44 -0500

    Home Page sends you to login if user not signed in!

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,52 @@
-import { VStack } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AddTask from '../components/AddTask';
 import TaskList from '../components/TaskList';
+import supabase from '../supabase';
+
+
 
 function HomePage() {
+
+  //React Router DOM
+  const navigate = useNavigate()  
+
+  //Supabase
+  const [user, setUser] = useState(undefined)
+  
+  useEffect(()=>{
+    getUserData()
+    if (!user === undefined) {
+      navigate('/')
+    } else{
+      navigate('/login')
+    }
+  },[])
+  const getUserData = async function(){
+    const {v} = await supabase.auth.getSession()
+
+    if (v !== undefined){setUser(v.user)}
+    
+  }
+
+  // const shouldIStayOrShouldIGo = function(){
+  //   getUserData()
+  //   if (!user === undefined) {
+  //     navigate('/')
+  //   } else{
+  //     navigate('/login')
+  //   }
+  // }
+    
   return (
-    <VStack justifyContent={'center'}>
-      <AddTask />
-      <TaskList />
-    </VStack>
+    <Center
+    h='80vh'
+    w='100vw'>
+      
+    
+
+    </Center>
   );
 }
 
