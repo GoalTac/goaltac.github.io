5c9d6e928c5a026e591dbf21f8a4f818e8c17a6d
Jack <jfernell123@gmail.com>
2022-12-31 20:56:08 +0000

    Layout fixes
    -Fixed image sizing and layout.
    -Made page more responsive.
    -Wrapped page in a simple container (Prob best to move container to index/app so dont need to copy container for each page).

--- src/pages/BetaPages/Hero.js
+++ src/pages/BetaPages/Hero.js
@@ -9,12 +9,12 @@ import {
   Text,
   useColorMode,
   Stack,
-  Image
+  Image,
 } from '@chakra-ui/react';
 import React from 'react';
 import supabase from '../../supabase';
 import { useState } from 'react';
-import createTask from "../../images/CreateTask.png"
+import createTask from '../../images/CreateTask.png';
 
 const Hero = () => {
   const toast = useToast();
@@ -28,7 +28,7 @@ const Hero = () => {
         .from('betaSignUp')
         .select()
         .eq('email', inputEmail);
-      
+
       if (betaSignUp.length > 0) {
         return toast({
           title: 'You are already signed up 🚀',
@@ -41,7 +41,7 @@ const Hero = () => {
         const { data, error } = await supabase
           .from('betaSignUp')
           .insert([{ email: inputEmail }]);
-        
+
         console.log(error);
         return toast({
           title: 'Message sent!🚀',
@@ -57,67 +57,72 @@ const Hero = () => {
   };
 
   return (
-    <Box padding="50">
+    <Flex
+      padding="50"
+      gap={15}
+      justifyContent="space-between"
+      alignItems="center"
+      flexWrap={'wrap'}
+    >
       <Box>
         <Heading
-                fontWeight="extrabold"
-                fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}
-                bg={colorMode === 'dark' ? 'white' : 'gray.700'}
-                bgClip="text"
-              >
-                IMPROVE YOUR WORK ETHIC WITH
-              </Heading>
+          fontWeight="extrabold"
+          fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}
+          bg={colorMode === 'dark' ? 'white' : 'gray.700'}
+          bgClip="text"
+        >
+          IMPROVE YOUR WORK ETHIC WITH
+        </Heading>
 
-              <Heading
-                fontWeight="extrabold"
-                bgGradient="linear(to-l, teal.300, blue.500)"
-                fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-                bgClip="text"
-              >
-                GoalTac
-              </Heading>
-              <FormControl
-                marginTop="200"
-                width={{ base: '100%', sm: '100%', md: '50%' }}
-              >
-                <Flex flexDirection="row" textAlign="center" alignItems="center">
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder=" Email Address "
-                    border="1px"
-                    borderRadius="2"
-                    marginRight="5"
-                    padding="3"
-                    _placeholder={{ color: 'inherit' }}
-                    value={inputEmail}
-                    onChange={event => setEmail(event.target.value)}
-                  />
-                  <Button
-                    borderRadius={5}
-                    width="30%"
-                    type="submit"
-                    fontSize="xs"
-                    border="1px"
-                    variant="solid"
-                    onClick={submitForm}
-                  >
-                    Sign Up
-                  </Button>
-                </Flex>
-                <Text color={colorMode === 'dark' ? 'white' : 'gray.700'} padding="3">
-                  Sign up for a chance to be selected for exclusive access! <br />
-                  <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
-                    Learn more
-                  </Button>
-                </Text>
-              </FormControl>
+        <Heading
+          fontWeight="extrabold"
+          bgGradient="linear(to-l, teal.300, blue.500)"
+          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+          bgClip="text"
+        >
+          GoalTac
+        </Heading>
+        <FormControl
+          marginTop="200"
+          width={{ base: '100%', sm: '100%', md: '50%' }}
+        >
+          <Flex flexDirection="row" textAlign="center" alignItems="center">
+            <Input
+              id="email"
+              type="email"
+              placeholder=" Email Address "
+              border="1px"
+              borderRadius="2"
+              marginRight="5"
+              padding="3"
+              _placeholder={{ color: 'inherit' }}
+              value={inputEmail}
+              onChange={event => setEmail(event.target.value)}
+            />
+            <Button
+              borderRadius={5}
+              width="30%"
+              type="submit"
+              fontSize="xs"
+              border="1px"
+              variant="solid"
+              onClick={submitForm}
+            >
+              Sign Up
+            </Button>
+          </Flex>
+          <Text color={colorMode === 'dark' ? 'white' : 'gray.700'} padding="3">
+            Sign up for a chance to be selected for exclusive access! <br />
+            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+              <a href="#about"> Learn more</a>
+            </Button>
+          </Text>
+        </FormControl>
       </Box>
-      <Box>
-        <Image src={createTask} w="100%" h="100%" />
+      <Box maxWidth={450} maxHeight={350}>
+        <Image src={createTask} boxShadow={'2xl'} borderRadius="lg" />
       </Box>
-      
-    </Box>
+    </Flex>
   );
 };
 
