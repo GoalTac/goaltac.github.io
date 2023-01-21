2356888cb26c55bf0e7b448d0740a1895456e7c1
Wryz <57204327+Wryz@users.noreply.github.com>
2023-01-21 18:27:38 -0500

    Psuedo-Code For New SignUp

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -12,10 +12,12 @@ import {
   FormControl,
   InputRightElement,
   useColorMode,
+  useToast, 
 } from '@chakra-ui/react';
 import { FaUserAlt, FaLock } from 'react-icons/fa';
 import supabase from '../supabase';
 import { useNavigate } from 'react-router-dom';
+import { AuthApiError } from '@supabase/supabase-js';
 
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
@@ -27,22 +29,83 @@ export default function SignUpPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [userName, setUserName] = useState('');
+  const toast = useToast();
 
   const handleShowClick = () => setShowPassword(!showPassword);
+
+
+  //Check for an existing userName
+  //Pass the email and password into Supabase Signup Method
+  //-- Display any issues with sign up to the user
+  //(Must check that account has been made) Insert username and other data into profiles table
   const handleSubmit = async event => {
     event.preventDefault();
     console.log('submitting!');
+    isValidUserName();
+    
     try {
       const { data, error } = await supabase.auth.signUp({ email, password });
+      console.log("Auth.signup returns: ", data, error);
+
+      if (isValidUserName()) {
+          //add profile data to the profiles table
+      }
+
+      //Display that an email for authentication has been sent to their email
 
       if (error) throw error;
+      return toast({
+        title: 'Account being created',
+        description:
+          "We've sent a verification link to your email from supabase.io.",
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      });
 
-      navigate('/');
     } catch (error) {
-      // Handle the error
+
+
       console.log(error);
+      return toast({
+        title: 'Authentication Error',
+        description: error.message,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
     }
   };
+  
+  const isValidSignUp = async event => {
+    let { data: emailQuery, error } = await supabase
+      .from('users')
+      .select()
+      .eq('email', email);
+    console.log(emailQuery)
+
+    //Display a toast if email already exists
+    if (emailQuery != null) {
+      console.log("Caught returning user!");
+    }
+  }
+
+  const isValidUserName = async event => {
+    let { data: userNameQuery, error } = await supabase
+      .from('profiles')
+      .select()
+      .eq('username', userName);
+    console.log(userNameQuery)
+
+    //Throw a toast if username does not pass char checks
+    //Throw a toast if username already exists
+    //Throw a toast if username is too long or too short
+    if (userNameQuery.length > 0) {
+      console.log("Caught returning user!");
+      return false;
+    }
+    return true;
+  }
 
   return (
     <Flex
@@ -66,7 +129,7 @@ export default function SignUpPage() {
           GoalTac Sign Up
         </Heading>
         <Box>
-          <form onSubmit={handleSubmit} _autofill={false}>
+          <form onSubmit={handleSubmit}>
             <Stack
               spacing={4}
               p="1rem"
