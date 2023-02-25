84a28d5a880e88cf776a78d98922ce6265861273
aditya-chandraker <aditya.chandraker@uconn.com>
2023-02-25 14:25:29 -0500

    communitySide

--- src/pages/SocialPage.js
+++ src/pages/SocialPage.js
@@ -8,15 +8,19 @@ import {
   useColorModeValue,
   Image,
   Spacer,
+  Heading,
+  Text,
 } from '@chakra-ui/react';
 import Explore from '../components/SocialPages/Explore';
 import Sidebar from '../components/SocialPages/Sidebar';
+import CommunitySide from '../components/SocialPages/CommunitySide';
 
 export default function SocialPage() {
   return (
     <Flex w='100%' h='100%' flexWrap={'wrap'} padding='10' gap={15}>
       <Sidebar />
       <Explore />
+      <CommunitySide />
     </Flex>
   );
 }
