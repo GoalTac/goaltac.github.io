7e207680cbf6cfab3aa1fae7f0c5753c54f71bea
Nikhil Ghosh <nikhil.ghosh@uconn.edu>
2023-03-02 21:31:39 -0500

    Refactored Messages

--- src/components/MessagerPages/MsgConversation.js
+++ src/components/MessagerPages/MsgConversation.js
@@ -1,25 +1,62 @@
+import SendMessage from './SendMessage';
+import supabase from '../../supabase';
+import { useState, useEffect } from 'react';
 import {
-  Flex,
-  FormControl,
-  HStack,
-  Input,
-  InputGroup,
+  VStack,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Icon,
   Text,
 } from '@chakra-ui/react';
+import { FaUserCircle } from 'react-icons/fa';
 
 export default function MsgConversation() {
+  const [user, setUser] = useState(null); //sets user ID not user list
+  const [message, setMessage] = useState({
+    sender_id: null,
+    recipient_id: null,
+    text: '',
+  });
+
+  async function fetchData() {
+    let { data: message, error } = await supabase.from('messages').select('*');
+    setMessage(message);
+    print('Messages are: \n', message);
+  }
+
+  // Bug: this useEffect is causing the page to try printing for some reason
+  // useEffect(() => {
+  //   fetchData();
+  //   print("is this causing print")
+  // }, []);
+
+  // useEffect(() => {
+  //   if (session == null) {
+  //     navigate('/beta');
+  //   } else {
+  //     setUser(session.user.id);
+  //   }
+  // }, [user]);
+
   return (
     <>
-      <Flex>
-        <HStack>
-          <Text>Message history</Text>
-          <FormControl>
-            <InputGroup>
-              <Input placeholder='send message' />
-            </InputGroup>
-          </FormControl>
-        </HStack>
-      </Flex>
+      <VStack id='Conversation' w='100%' h='100%'>
+        <Card w='95%' h='95%' border='1px'>
+          <CardHeader align='center'>
+            <Icon as={FaUserCircle} boxSize={5} />
+            Person X
+          </CardHeader>
+          <CardBody verticalAlign={'bottom'}>
+            (message history with person)
+            <Text>Newer message</Text>
+          </CardBody>
+          <CardFooter>
+            <SendMessage userid={user}/>
+          </CardFooter>
+        </Card>
+      </VStack>
     </>
   );
 }
--- src/components/MessagerPages/MsgSidebar.js
+++ src/components/MessagerPages/MsgSidebar.js
@@ -1,30 +1,97 @@
-import { Box, HStack, VStack } from '@chakra-ui/react';
+import supabase from '../../supabase';
 import { useEffect, useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import {
+  VStack,
+  Text,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Icon,
+  FormControl,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
+import { FaSearch, FaUserCircle } from 'react-icons/fa';
+import { BiMessageEdit } from 'react-icons/bi';
 
-// This isn't working for some reason when I try to import
 export default function MsgSidebar() {
-  const state = useState();
+  const [search, setSearch] = useState('');
+  const [contacts, setContacts] = useState([]);
+
+  // Get contacts history data from Supabase
+  async function fetchData() {
+    // let { data: contacts, error } = await supabase.from('messages').select('*');
+    // setContacts(contacts);
+    // print("Contacts are: \n", contacts);
+  }
+
+  // Bug: this useEffect is causing the page to try printing for some reason
+  // useEffect(() => {
+  //   // fetchData();
+  //   print("is this causing print")
+  // }, []);
+
+  const openConvo = async event => {
+    console.log('Open convo with: ', event);
+    // switch the MsgConversation() to be for the friend clicked on
+  };
+
+  const searchPeople = async event => {
+    console.log('searching for ', event);
+  };
+
+  // If the user has no messages, display something different
+  //   if (!contacts || !contacts.length) {
+  //     return (
+  //       <Box align='center'>
+  //         <Text>No messages yet!</Text>
+  //       </Box>
+  //     );
+  //   }
+
   return (
     <>
-      <Box>
-        <HStack>Messages</HStack>
-        <VStack>
-          <FormControl>
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents='none'
-                children={<FaSearch color='gray.300' />}
-              />
-              <Input id='search' placeholder='search' />
-            </InputGroup>
-          </FormControl>
-          <Button>Person 1</Button>
-          <Button>Person 2</Button>
-          <Button>Person 3</Button>
-          <Button>Person 4</Button>
-        </VStack>
-      </Box>
+      <VStack id='Sidebar'>
+        <FormControl>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents='none'
+              children={<FaSearch color='gray.300' />}
+            />
+            <Input
+              placeholder='search for person'
+              type='text'
+              value={search}
+              onChange={e => setSearch(search)}
+            />
+            <Button>
+              <Icon as={BiMessageEdit} boxSize={8} onClick={openConvo} />
+            </Button>
+          </InputGroup>
+        </FormControl>
+
+        {['Person 1', 'Person 2', 'Person 3'].map(
+          (person) => ( // this line will be {contacts.map((person)... after fetching Supabase data
+            <Card
+              key={person}
+              w='100%'
+              h='15%'
+              onClick={openConvo}
+              border='1px'
+            >
+              <CardHeader fontSize='14px'>
+                <Icon as={FaUserCircle} boxSize={4} />
+                {person}
+              </CardHeader>
+              <CardBody fontSize={'12px'}>
+                <Text>preview with: {person}.. </Text>
+              </CardBody>
+            </Card>
+          )
+        )}
+      </VStack>
     </>
   );
 }
