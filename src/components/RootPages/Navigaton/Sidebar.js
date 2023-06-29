ce630fec91882c8fcf637dd15c4ea296c3b88241
Wrys <wrysplays@gmail.com>
2023-06-29 13:17:10 -0400

    Sidebar: Elongates sidebar to fill page

--- src/components/RootPages/Navigaton/Sidebar.js
+++ src/components/RootPages/Navigaton/Sidebar.js
@@ -37,7 +37,7 @@ export const Sidebar = ({activeItem, highBarItems, lowBarItems}) => {
 
     return (
         <Box left='0'
-            posiiton='fixed'
+            posiiton='sticky'
             top='0'
             boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.1)">
         <Flex className='SideBar Parent' id='SideBar Parent'
@@ -45,66 +45,69 @@ export const Sidebar = ({activeItem, highBarItems, lowBarItems}) => {
             backgroundColor={(colorMode == 'dark' ? 'gray.700' : 'white')}
             minH='100vh'
             maxWidth='15vw'
-            width='min-content'
+            width='max-content'
             alignItems='center'
             flexDir="column">
-
-            {/* How to prevent logo resizing? */}
-            <Box className='SideBar Parent Logo' id='SideBar Parent Logo'
-                margin='0.5rem' 
-                marginLeft={navSize == 'small' ? '1rem' : null}
-                boxSize={navSize == 'small' ? '50%' : '90%'}>
-                <Image className='Sidebar Logo' id='SideBar Logo'                
-                    src={navSize == 'small' ? smalllogo : largelogo}/>
-            </Box>
-            
-            
-            <VStack className='SideBar Parent Contents' id='SideBar Parent Contents'
-                marginX={navSize == 'small' ? '0.5rem' : '1.5rem'}
-                justify='space-between'
-                flexDir='column'>
-                <Box className='SideBar Nav Content' id='SideBar Nav Content'>
-                    {highBarItems.map((item, index) => {
-                        return (
-                            <Box key={index} marginY='0.5rem'>
-
-                                <NavItem
-                                    margin='0.5rem'
-                                    
-                                    navSize={navSize} 
-                                    icon={item.icon} 
-                                    title={item.title}
-                                    description={item.description}
-                                    nav={item.nav}
-                                    active={activeItem == item.nav ? true : false}/>
-                            </Box>
-                        );
-                    })}
-                    <Divider orientation='horizontal' size='lg' colorScheme='black'/>
-                    {lowBarItems.map((item, index) => {
-                        return (
-                            <Box key={index} marginY='0.5rem'>
-
-                                <NavItem
-                                    margin='0.5rem'
-                                    
-                                    navSize={navSize} 
-                                    icon={item.icon} 
-                                    title={item.title}
-                                    description={item.description}
-                                    nav={item.nav}
-                                    active={activeItem == item.nav ? true : false}/>
-                            </Box>
-                        );
-                    })}
-                </Box>            
-                    
-                <Divider />
-                <Box className='SideBar Settings' id='SideBar Settings'
-                    marginY='1rem'>
-                    <Settings session={useSupabaseClient.session} />
+            <Flex alignItems='center' flexDir="column" height='max-content' paddingX='2px'>
+                {/* How to prevent logo resizing? */}
+                <Box className='SideBar Parent Logo' id='SideBar Parent Logo'
+                    margin='0.5rem' 
+                    marginLeft={navSize == 'small' ? '1rem' : null}
+                    boxSize={navSize == 'small' ? '50%' : '90%'}>
+                    <Image className='Sidebar Logo' id='SideBar Logo'                
+                        src={navSize == 'small' ? smalllogo : largelogo}/>
                 </Box>
-            </VStack>
+                
+                
+                <VStack className='SideBar Parent Contents' id='SideBar Parent Contents'
+                    marginX={navSize == 'small' ? '0.5rem' : '1.5rem'}
+                    justify='space-between'
+                    flexDir='column'>
+                    <Box className='SideBar Nav Content' id='SideBar Nav Content'>
+                        {highBarItems.map((item, index) => {
+                            return (
+                                <Box key={index} marginY='0.5rem'>
+
+                                    <NavItem
+                                        margin='0.5rem'
+                                        
+                                        navSize={navSize} 
+                                        icon={item.icon} 
+                                        title={item.title}
+                                        description={item.description}
+                                        nav={item.nav}
+                                        active={activeItem == item.nav ? true : false}/>
+                                </Box>
+                            );
+                        })}
+                        <Divider orientation='horizontal' size='lg' colorScheme='black'/>
+                        {lowBarItems.map((item, index) => {
+                            return (
+                                <Box key={index} marginY='0.5rem'>
+
+                                    <NavItem
+                                        margin='0.5rem'
+                                        
+                                        navSize={navSize} 
+                                        icon={item.icon} 
+                                        title={item.title}
+                                        description={item.description}
+                                        nav={item.nav}
+                                        active={activeItem == item.nav ? true : false}/>
+                                </Box>
+                            );
+                        })}
+                    </Box>            
+                        
+                    <Divider />
+                    <Box className='SideBar Settings' id='SideBar Settings'
+                        marginY='1rem'>
+                        <Settings session={useSupabaseClient.session} />
+                    </Box>
+                </VStack> 
+            </Flex>
+
+            
         </Flex>
         </Box>
         
