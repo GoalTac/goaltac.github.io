27f8a6312d0862ac06e8aa213115e0c4ecba3faa
Wrys <wrysplays@gmail.com>
2023-06-29 15:32:09 -0400

    Roster: Rough draft of Roster

--- src/components/Communities/InsideView/Group/Roster.js
+++ src/components/Communities/InsideView/Group/Roster.js
@@ -1,15 +1,33 @@
 import {
     Box,
+    Text,
+    Flex,
+    Heading,
+    Spacer,
+    Icon,
+    HStack,
+    Divider,
+    useColorMode
 } from '@chakra-ui/react'
+import {
+    GiArrowhead,
+    GiPerson
+  
+  } from 'react-icons/gi'
+import { formatNumber } from '../../../../hooks/Utilities/Numbers';
+
 //roster has different viewables based on permission level
 /**
  * Displays the clan's roster
  * @param {*} param0
  */
 export default function Roster({people}) {
-    return (
-        <Box borderWidth='1px' height='10px'></Box>
-    )
+
+    const orderedPeople = numericalOrder({people})
+
+    return (<Box minHeight='100%' paddingTop='20px'>
+        {orderedPeople}
+    </Box>)
     
 }
 
@@ -31,18 +49,12 @@ function checkPermission(user) {
  * @param {*} param0 
  * @returns ordered list of every member
  */
-function numbericalOrdererRoster({people}) {
-
-    const orderedMembers = people.sort((a, b) => (a.points > b.points) ? 1 : -1)
-
-
-    return (<Box>
-        {people.map((index, member) => (
-            <Box key={index}>
-                {}
-            </Box>
-        ))}
-    </Box>)
+function numericalOrder({people}) {
+    const orderedPeople = people.sort((a, b) => (a.points < b.points) ? 1 : -1)
+    return orderedPeople.map((personObj, index) => (
+        <RowItem key={index} 
+        number={index}
+        member={personObj}/>))
 }
 
 /**
@@ -51,8 +63,41 @@ function numbericalOrdererRoster({people}) {
  * @param {*} param0 
  * @returns Row display for roster
  */
-function rowItem({member}) {
-    return (<Box>
+function RowItem({member, number}) {
+    
+    const { colorMode } = useColorMode();
 
-    </Box>);
+
+    return (
+    <Flex height='60px' 
+        marginY='5px'
+        flexDirection='column'
+        borderWidth='1px'
+        borderRadius='15px'
+        paddingX='1rem'>
+        <HStack width='100%' justifyContent='center' marginY='auto'>
+            <Heading fontSize='1.5rem' paddingEnd='1rem'>
+                {number + 1}.
+            </Heading>
+            <Divider orientation='vertical' />
+            <Text fontSize='1.5rem' paddingStart='0.5rem'>
+                {member.userName}
+            </Text>
+            <Spacer/>
+            <Flex borderColor='blue.200' 
+                backgroundColor={colorMode == 'dark' ? 'blue.700' : 'blue.100'}
+                borderRadius='10px' 
+                width='120px'
+                padding='4px' 
+                columnGap='10px'
+                justifyContent='right'
+                borderWidth='2px'>
+                <Text fontSize='1.25rem'>
+                    {formatNumber(member.points)}
+                </Text>
+                <Icon boxSize='1.5rem' as={GiArrowhead}/>
+            </Flex>
+
+        </HStack>
+    </Flex>);
 }
