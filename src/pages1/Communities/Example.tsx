0468eb6cb8d11bc87adeec96a2730f1c03c5896b
Wrys <wrysplays@gmail.com>
2023-07-07 12:44:15 -0400

    ExampleCommunity: Created exp. static communities

--- src/pages1/Communities/Example.tsx
+++ src/pages1/Communities/Example.tsx
@@ -5,27 +5,32 @@ export const exampleCommunity = {
     members: [
         {
             uuid: 'cf636296-2c08-4a41-94dc-77b5518ba267',
-            userName: 'Adi',
             points: 1209,
     
         },
         {
             uuid: 'd0ab045d-568d-409a-91d4-b09bb5805ce6',
-            userName: 'My',
             points: 312
         },
         {
             uuid: 'something!',
-            userName: 'Nikhil',
             points: 12019
         },
         {
             uuid: 'woo woo',
-            userName: 'Andrew',
             points: 19
         },
     ],
-    goals: [12, 142, 124, 29, 93], //id numbers of the goals (which consists of tasks)
+    goals: [
+        {
+            goalID: 12,
+            progress: 5
+        },
+        {
+            goalID: 142,
+            progress: 10
+        }
+    ], //id numbers of the goals (which consists of tasks)
     goalsC: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19],
     totalPoints: 11210, //this will be taken out later and we can calculate total points by looping through each member and getting their point value
     stats: {
