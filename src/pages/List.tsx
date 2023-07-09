de6c97c4322624ecf4b1f82d574ddfde7b6e7e1d
Wrys <wrysplays@gmail.com>
2023-07-08 20:34:45 -0400

    List: Added getTasks()

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,35 @@
-import {Box} from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { supabase } from './../supabase'
 
 export default function List() {
+
+    async function getTasks() {
+        const { data: taskData, error } = await supabase
+            .from('todos')
+            .select('*');
+
+        if (error) {
+            console.error(error);
+            return;
+        }
+        return taskData;
+    }
+
+    /**
+     * 
+    
+    const [community, setCommunity] = useState<any>();
+
+    useEffect(() => {
+        const fetchCommunityData = async() => getCommunity(communityName).then((response) => {
+            setCommunity( response );
+        })
+        fetchCommunityData();
+    }, []);
+
+     */
+
     return(<Box>
-        
+
     </Box>)
 }
