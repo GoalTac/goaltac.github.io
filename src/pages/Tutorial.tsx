c13961a6e6a746c09b171c140203d3a0a115ca6e
Wrys <wrysplays@gmail.com>
2023-08-24 10:47:00 -0400

    Tutorial: Created biography

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,6 +1,4 @@
-import { AtSignIcon } from "@chakra-ui/icons";
 import { Box, Button, Flex, FormControl, Heading, Image, Input, InputGroup, InputLeftElement, InputRightElement, Link, Text, Stack, FormHelperText, FormLabel, useToast } from "@chakra-ui/react";
-import Canvas from "../components/Canvas";
 import logo from './../images/GoalTac_TLogo.png'
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -10,6 +8,7 @@ import { useSession } from "../hooks/SessionProvider";
 export function Tutorial() {
     const [userName, setUserName] = useState('');
     const [displayName, setDisplayName] = useState('');
+    const [biography, setBiography] = useState('');
 
     const toast = useToast();
     const navigate = useNavigate();
@@ -53,28 +52,48 @@ export function Tutorial() {
         } 
         return (error == null ? true : false)
       };
-    
-      const handleSubmit = () => {
-        const checkOne = handleDisplaySubmit()
-        const checkTwo = handleNameSubmit()
 
-        Promise.all([checkOne, checkTwo]).then((checks: any)=>{
-            if(!checks.every((e: boolean) => e === true)) {
-                setDisplayName('')
-                setUserName('')
-            } else {
-              toast({
-                  title: `Your profile was created!`,
-                  status: 'success',
-                  duration: 3000,
-                  isClosable: true,
-                });
-              navigate('/dashboard')
-              window.location.reload(); //I needed to do this for some reason. Nav didn't want to reload the page
-            }
-        })
-        
-      }
+    const handleBiographySubmit = async () => {
+      // get the user's information    
+      const { data, error } = await supabase
+        .from('profiles')
+        .update({ biography: biography})
+        .eq('userid', userID) //could have more protections behind this
+        .select()
+      if (error) {
+        console.log(error)
+        toast({
+          title: `There was an error with your biography!`,
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        });
+      } 
+      return (error == null ? true : false)
+    };
+    
+    const handleSubmit = () => {
+      const checkOne = handleDisplaySubmit()
+      const checkTwo = handleNameSubmit()
+      const checkThree = handleBiographySubmit()
+
+      Promise.all([checkOne, checkTwo, checkThree]).then((checks: any)=>{
+          if(!checks.every((e: boolean) => e === true)) {
+              setDisplayName('')
+              setUserName('')
+              setBiography('')
+          } else {
+            toast({
+                title: `Your profile was created!`,
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+              });
+            navigate('/dashboard')
+            window.location.reload(); //I needed to do this for some reason. Nav didn't want to reload the page
+          }
+      })
+    }
 
     return (
         <Flex alignItems='center' flexDirection='column' paddingBottom='60px'>
@@ -83,7 +102,7 @@ export function Tutorial() {
                 One last thing...
             </Heading>
             <FormControl  maxWidth={['90%','60%']}>
-                <Flex flexDirection='column' paddingY='40px'>
+                <Flex flexDirection='column' paddingY='20px'>
                     <FormLabel fontSize='1.5rem' fontWeight='300'>
                         Please choose your username
                     </FormLabel>
@@ -99,18 +118,31 @@ export function Tutorial() {
                     </FormHelperText>
                 </Flex>
                 
-                <Flex flexDirection='column' paddingY='40px'>
+                <Flex flexDirection='column' paddingY='20px'>
                     <FormLabel fontSize='1.5rem' fontWeight='300'>
                         Please choose your display name
                     </FormLabel>
                     <Input isRequired type='text' fontSize='2rem' paddingY='30px' paddingX='20px'
                         value={displayName}
                         onInput={(e) => {
-                        e.currentTarget.value = e.currentTarget.value.replace(/[^A-z0-9_ ]/g, '');
-                        }}
+                        e.currentTarget.value = e.currentTarget.value.replace(/[^A-z0-9_ ]/g, ''); }}
                         onChange={(e) => setDisplayName(e.target.value)}/>
                     <FormHelperText>
                         What's your name?
+                    </FormHelperText>
+                </Flex>
+
+                <Flex flexDirection='column' paddingY='20px'>
+                    <FormLabel fontSize='1.5rem' fontWeight='300'>
+                        Biography
+                    </FormLabel>
+                    <Input isRequired type='text' fontSize='2rem' paddingY='30px' paddingX='20px'
+                        value={biography}
+                        onInput={(e) => {
+                        e.currentTarget.value = e.currentTarget.value.replace(/[^A-z0-9_ ]/g, ''); }}
+                        onChange={(e) => setBiography(e.target.value)}/>
+                    <FormHelperText>
+                        Tell me about yourself
                     </FormHelperText>
                 </Flex>
             </FormControl>
