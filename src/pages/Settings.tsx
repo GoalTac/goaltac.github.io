71c38fce13147babc270fb20606e4ed9ae674180
Nikhil Ghosh <nikhil.ghosh@uconn.edu>
2023-09-01 16:30:50 -0400

    commented friends functionality

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -217,6 +217,7 @@ export default function Profile() {
         //get user profile
         const { data: {user} } = await supabase.auth.getUser()
 
+        // get the profile of the friend being added
         const { data: friend, error: friendError} = await supabase
             .from('profiles')
             .select('userid')
@@ -280,6 +281,7 @@ export default function Profile() {
     const handleAcceptFriendRequest = async (request: FriendRequestType) => {
         const { data: { user } } = await supabase.auth.getUser();
     
+        // get the profile of the friend being added
         const { data: friend, error: friendError } = await supabase
             .from('profiles')
             .select('userid')
