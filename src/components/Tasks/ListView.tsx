e606c11bdeeea90292ee7e565438fafea5ded094
Wrys <wrysplays@gmail.com>
2023-10-06 17:29:50 -0400

    Added Information Modal to display collaborators

--- src/components/Tasks/ListView.tsx
+++ src/components/Tasks/ListView.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { supabase } from "../../supabase";
 import { now } from "lodash";
 import { start } from "repl";
-import { CalendarIcon, CheckCircleIcon } from "@chakra-ui/icons";
+import { CalendarIcon, CheckCircleIcon, InfoIcon } from "@chakra-ui/icons";
 import { FaHourglass, FaHourglassHalf, FaHourglassStart } from "react-icons/fa";
 import { Task, _addPost, _addProgress, _deleteProgress, _setProgress } from "./TaskAPI";
 import TaskDrawer from "./TaskDrawer";
@@ -34,8 +34,9 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
 
         const [progress, setProgress ] = useState<number>(task.progress) //how much progress the user has made
         const totalProgress = task.all_progress ? task.all_progress-task.progress : 0 //how much progress has been made on the task total
+        const { isOpen: progressIsOpen, onOpen: progressOnOpen, onClose: progressOnClose } = useDisclosure()
+        const { isOpen: infoIsOpen, onOpen: infoOnOpen, onClose: infoOnClose } = useDisclosure()
 
-        const { isOpen, onOpen, onClose } = useDisclosure()
         const colorTheme = {
             inComplete: {
                 dark: 'red.600',
@@ -111,7 +112,7 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
             function SimpleCompletion() {
                 const [newProgress, setNewProgress ] = useState<any>(progress)
                 const handleSave = async() => {
-                    onClose()
+                    progressOnClose()
                     setProgress(newProgress)
         
                     await _setProgress(task.user_id, task.task_id, newProgress)
@@ -137,7 +138,7 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
                     </Grid>
                 }
         
-                return <Modal scrollBehavior='inside' isCentered motionPreset='slideInBottom' closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                return <Modal scrollBehavior='inside' isCentered motionPreset='slideInBottom' closeOnOverlayClick={false} isOpen={progressIsOpen} onClose={progressOnClose}>
                     <ModalOverlay />
                     <ModalContent backgroundColor={useColorModeValue('gray.50','gray.800')}>
                     <ModalHeader>Set your Progress</ModalHeader>
@@ -166,7 +167,7 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
                        
                     <ModalFooter>
                         <Button type='submit' colorScheme='blue' mr={3} onClick={handleSave}>Save</Button>
-                        <Button variant='outline' onClick={onClose}>Cancel</Button>
+                        <Button variant='outline' onClick={progressOnClose}>Cancel</Button>
                     </ModalFooter>
                     </ModalContent>
                 </Modal>
@@ -174,10 +175,8 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
         
             function ProgressCompletion() {
                 const [newProgress, setNewProgress ] = useState<number>(progress) //variably changes
-                
-                console.log(newProgress, totalProgress, progress)
                 const handleSave = async() => {
-                    onClose()
+                    progressOnClose()
                     setProgress(newProgress)
                     setNewProgress(0)
 
@@ -191,7 +190,7 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
                         isClosable: true,
                     })
                 }
-                return <Modal scrollBehavior='inside' isCentered motionPreset='slideInBottom' closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                return <Modal scrollBehavior='inside' isCentered motionPreset='slideInBottom' closeOnOverlayClick={false} isOpen={progressIsOpen} onClose={progressOnClose}>
                     <ModalOverlay />
                     <ModalContent backgroundColor={useColorModeValue('gray.50','gray.800')}>
                     <ModalHeader>Set your Progress</ModalHeader>
@@ -226,7 +225,7 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
                        
                     <ModalFooter>
                         <Button type='submit' colorScheme='blue' mr={3} onClick={handleSave}>Save</Button>
-                        <Button variant='outline' onClick={onClose}>Cancel</Button>
+                        <Button variant='outline' onClick={progressOnClose}>Cancel</Button>
                     </ModalFooter>
                     </ModalContent>
                 </Modal>
@@ -242,28 +241,89 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
             }
         }
 
+        function InfoModal() {
+            const totalPercentProgress: number = ((task.all_progress/requirement) * 100)
+
+            return <Modal scrollBehavior='inside' isCentered motionPreset='slideInBottom' closeOnOverlayClick={false} isOpen={infoIsOpen} onClose={infoOnClose}>
+                <ModalOverlay />
+                <ModalContent backgroundColor={useColorModeValue('gray.50','gray.800')}>
+                <ModalHeader>Collaborator Information</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Stack flexDirection='column' marginBottom='20px'>
+                        <Heading fontSize='16px'>Total Progress</Heading>
+                        <HStack fontSize={'12px'}>
+                            <Text>{((task.all_progress/requirement) * 100).toFixed(2)}%</Text>
+                            <Spacer/>
+                            <Text>{task.all_progress}/{requirement}</Text>
+                        </HStack>
+                        <Progress size='lg' borderRadius='full' marginLeft='auto' width='full' value={totalPercentProgress > 0 ? totalPercentProgress : 1} backgroundColor={useColorModeValue('gray.200','gray.600')} colorScheme={totalPercentProgress >= requirement ? 'green' : 'orange'}/>
+                    </Stack>
+
+                    <Stack flexDirection='column' spacing='1rem'>
+
+                    {collaborators.map((collaborator: any, id: number)=>{
+                        const percentProgress: number = ((collaborator.progress/requirement) * 100)
+                        return <Card padding='10px' key={id}><HStack paddingY='10px'>
+                            <VStack>
+                               <Avatar cursor='pointer' size='md' borderWidth='0px' key={collaborator.userName} onClick={()=>navigate(`/profile/${collaborator.userName}`)} name={collaborator.displayName} src={collaborator.avatarURL} />
+                                <Text>{collaborator.displayName}</Text>
+                            </VStack>
+                            <Spacer/>
+                            <Stack flexDirection='column'>
+                                <HStack fontSize={'12px'}>
+                                    <Text>{((collaborator.progress/requirement) * 100).toFixed(2)}%</Text>
+                                    <Spacer/>
+                                    <Text>{collaborator.progress}/{requirement}</Text>
+                                </HStack>
+                                <Progress size='lg' borderRadius='full' marginLeft='auto' width='200px' value={percentProgress > 0 ? percentProgress : 1} backgroundColor={useColorModeValue('gray.200','gray.600')} colorScheme={progress >= requirement ? 'green' : 'orange'}/>
+                            </Stack>
+                            
+                        </HStack></Card>
+                    })}</Stack>
+                </ModalBody>
+                    
+                <ModalFooter>
+                    <Button variant='outline' onClick={infoOnClose}>Cancel</Button>
+                </ModalFooter>
+                </ModalContent>
+            </Modal>
+        }
+
         return <Card backgroundColor={useColorModeValue('gray.50','gray.700')} margin='20px' overflow='hidden' height='200px' width='280px' size='md' flexDirection={'column'} alignItems={[null,'center']} >
             
             {!isCollaborative ? <TaskDrawer preset={task}>
                 <Flex width='prose' height={'30px'} cursor='pointer' backgroundColor={pickedColor}/>
             </TaskDrawer> : 
             
-            (isOwner ? <TaskDrawer preset={task}>
-                <Box height='30px' cursor='pointer' width='full' backgroundColor={pickedColor} paddingY='auto'>
-                    <AvatarGroup marginStart='2px' height='inherit' size='xs' max={5} spacing='2px'>
-                        {collaborators.map((collaborator: any)=>{
-                            return <Avatar key={collaborator.userName} onClick={()=>navigate(`/profile/${collaborator.userName}`)} name={collaborator.displayName} src={collaborator.avatarURL} />
-                        })}
-                    </AvatarGroup>
-                </Box>
-            </TaskDrawer> :
-            <Box height='30px' cursor='pointer' width='full' backgroundColor={pickedColor} paddingY='auto'>
-                <AvatarGroup marginStart='2px' height='inherit' size='xs' max={5} spacing='2px'>
+            (isOwner ? <HStack height='30px' cursor='pointer' width='100%' backgroundColor={pickedColor} paddingY='auto'>
+                <TaskDrawer preset={task}>
+                    <HStack height='30px' width='280px'>                   
+                        
+                    </HStack>
+                </TaskDrawer>
+                <AvatarGroup marginStart='2px' height='inherit' size='xs' max={5} spacing='2px' position='absolute'>
                     {collaborators.map((collaborator: any)=>{
-                        return <Avatar key={collaborator.userName} onClick={()=>navigate(`/profile/${collaborator.userName}`)} name={collaborator.displayName} src={collaborator.avatarURL} />
+                        return <Avatar borderWidth='0px' key={collaborator.userName} onClick={()=>navigate(`/profile/${collaborator.userName}`)} name={collaborator.displayName} src={collaborator.avatarURL} />
                     })}
                 </AvatarGroup>
-            </Box>)}
+                <Box onClick={infoOnOpen} position='absolute' right='0'>
+                    <IconButton variant='unstyled' aria-label='information' icon={<InfoIcon/>}/>
+                    <InfoModal/>
+                </Box>
+            </HStack> :
+            <HStack height='30px' cursor='pointer' width='full' backgroundColor={pickedColor} paddingY='auto'>
+                <AvatarGroup marginStart='2px' height='inherit' size='xs' max={5} spacing='2px'>
+                    {collaborators.map((collaborator: any)=>{
+                        return <Avatar borderWidth='0px' key={collaborator.userName} onClick={()=>navigate(`/profile/${collaborator.userName}`)} name={collaborator.displayName} src={collaborator.avatarURL} />
+                    })}
+                </AvatarGroup>
+                <Spacer/>
+                <Box onClick={infoOnOpen}>
+                    <IconButton variant='unstyled' aria-label='information' icon={<InfoIcon/>}/>
+                    <InfoModal/>
+                </Box>  
+            </HStack>)}
             
             <Flex width='100%' flexDirection='column' padding='10px' height='inherit' alignItems={['center','start']}>
                 <Flex flexDirection={'column'} height='100%' width='100%'>
@@ -282,10 +342,10 @@ export default function ListView({tasks}: Task[] | any, {relations}: any) {
                 <Flex alignItems='end' width='100%'>
                     <ProgressIndicator/>
                     <Spacer/>
-                    <Box onClick={onOpen}>
+                    <Box onClick={progressOnOpen}>
                         <IconButton aria-label='Set Progress' backgroundColor={pickedColor} icon={<FaHourglassHalf />} type='button' variant='solid' />
                         <SwitchCompletion/>
-                    </Box>                    
+                    </Box>
                     
                 </Flex>
             </Flex>
