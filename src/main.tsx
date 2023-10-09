1bed0f08c216f962b37bdb92b61bf4899244b7d8
Wrys <wrysplays@gmail.com>
2023-10-09 12:57:22 -0400

    Removed React Strict Mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 import theme from './hooks/Theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ChakraProvider>
-  </React.StrictMode>,
 );
